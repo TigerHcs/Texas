6b9a81d9c89774519c8e20f7c42bdafd45911d2a
Tiger <hmm@pku.edu.cn>
2021-01-17 16:05:31 +0800

    class range_util

--- modules/texaspoker/AI/range_util.py
+++ modules/texaspoker/AI/range_util.py
@@ -1,9 +1,5 @@
 import pickle
 from collections import defaultdict
-import os
-
-origin_range_filename = "origin_range"
-flop_range_filename = "flop_range"
 
 def my_hash(cards):
     ret = 0
@@ -19,29 +15,36 @@ def my_dehash(v):
     ret.reverse()
     return ret
 
-with open(flop_range_filename, 'rb') as f:
-    flop_range = pickle.load(f)
-with open(origin_range_filename, 'rb') as f:
-    origin_range = pickle.load(f)
-    origin_range = origin_range[0]
+class RangeUtil():
+    def __init__(self):
+        self.origin_range_filename = "origin_range"
+        self.flop_range_filename = "flop_range"
+        with open(self.flop_range_filename, 'rb') as f:
+            self.flop_range = pickle.load(f)
+        with open(self.origin_range_filename, 'rb') as f:
+            self.origin_range = pickle.load(f)
+            self.origin_range = self.origin_range[0]
+    def update_range(self, my_hand, com, cur_range):
+        my_hand = sorted(my_hand)
+        com = sorted(com)
+        if not com: # before flop
+            return sorted(cur_range, key = lambda x:-self.origin_range[my_hash(x)])
+        elif len(com)==3: #flop
+            return sorted(cur_range, key = lambda x:-self.flop_range[my_hash(com)][my_hash(x)])
+        elif len(com)==4: #turn
+            return cur_range
+        elif len(com)==5: #river
+            return cur_range
+        else:
+            print("Invalid com cards!!!")
+            return cur_range
 
-def update_range(my_hand, com, cur_range, ratio):
-    my_hand = sorted(my_hand)
-    com = sorted(com)
-    if not com: # before flop
-        return sorted(cur_range, key = lambda x:-origin_range[my_hash(x)])
-    elif len(com)==3: #flop
-        return sorted(cur_range, key = lambda x:-flop_range[my_hash(com)][my_hash(x)])
-    elif len(com)==4: #turn
-        return cur_range
-    elif len(com)==5: #river
-        return cur_range
-    else:
-        print("Invalid com cards!!!")
-        return cur_range
 
-lst = []
-for k,v in origin_range.items():
-    lst.append([k,v])
-lst = sorted(lst, key =lambda x:-x[1])
-print([[my_dehash(x[0]), x[1]] for x in lst[:100]])
+if __name__ == "__main__":
+    range_util = RangeUtil()
+    lst = []
+    for i in range(52):
+        for j in range(i+1,52):
+            lst.append([i,j])
+    lst = range_util.update_range([],[8,9,10],lst)
+    print(lst[:100])
